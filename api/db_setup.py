1fc53ea1f73cfe49f745fa6427351d856db248c3
bouzenaali <a_bouzena@estin.dz>
2024-10-24 20:25:00 +0100

    fix: edit the database url to fix the connection with database problem

--- api/db_setup.py
+++ api/db_setup.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 # SQLAlchemy settings for SQLite database
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@fastapi_database:5432/student"
-
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@database:5432/student"
 # Create the SQLAlchemy engine
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
